80c18ac4f18e947b5b2eeb9cd07f0ab00ea966da
grad <grad@server0.home>
2016-12-01 21:40:39 -0200

    add delay

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <time.h>
 
 void bemVindo(); 	// prototipo da funcao de boas vindas
+void recebeVariaveis(); // prototipo da funcao input sequencia
 void sequencia();	// prototipo da funcao de sequencia
 void ajuda();		// prototipo da funcao de ajuda
 int main(){
@@ -13,7 +14,7 @@ int main(){
 	scanf("%d", &escolha);
 	switch(escolha){
 		case 1:
-			sequencia();
+			recebeVariaveis();
 			break;
 		case 2:
 			ajuda();
@@ -30,10 +31,11 @@ int main(){
 
 /** Funcao de boas vindas e exibicao do menu principal **/
 void bemVindo(){
-	system("title CONTROLE PORTA PARALELA");
-	system("cls");
-	printf("Bem-vindo ao controle de porta paralela\n");
-	printf("escolha\t\tfuncao\n\n");
+	system("title CONTROLE PORTA PARALELA"); // Muda o titulo da janela
+	system("cls"); // limpa a janela
+	printf("Bem-vindo ao controle de porta paralela\n"); // Mensagem de boas vindas
+	printf("escolha\t\tfuncao\n\n"); // Cabecalho do menu Inicial
+	/*** Itens do menu ***/
 	printf("1\t\tIniciar sequencia\n");
 	printf("2\t\tAjuda\n");
 	printf("3\t\tSair");
@@ -50,13 +52,25 @@ void ajuda(){
 	system("cls");
 }
 /** Funcao para definir a sequencia de LEDs acesos **/
-void sequencia(){
+void recebeVariaveis(){
+	int quantPassos = 0; // Variavel que ira receber a quantidade de passos
+	printf("Digite a quantidade de passos: "); // Mensagem com informacao para usuario
+	scanf("%d", &quantPassos); // Recebe o valor digitado, sem tratamento
+	int passos[quantPassos]; // Declara um array de tamanho "quantPassos"
+	int i; // Declara um contador
+	/*** Loop para zerar todas as posicoes do array ***/
+	for(i = 0; i < quantPassos; i++){
+		passos[i] = 0;
+	}
+	
+	int tempo = 0; // Variavel para receber o tempo entre cada passo
+
 }
 void atraso(int atraso){
 	int x;
-	clock_t Ini;  //Criação da variavel relógio
-	Ini = clock(); //Inicialização do relógio
-	while(((clock() - Ini) / (CLOCKS_PER_SEC / 100)) <= 10){  //Executa o while por 10ms antes de retornar o valor
+	clock_t Ini;  //Criacao da variavel relogio
+	Ini = clock(); //Inicializacao do relogio
+	while(((clock() - Ini) / (CLOCKS_PER_SEC / 100)) <= atraso){  //Executa o while por 10ms antes de retornar o valor
 			x++;
 	}
 }
