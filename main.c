9083dd8af9ef3460930de1daa82e7be8d9e7682b
alexNeto <alex.Neto3@hotmail.com>
2016-12-03 13:11:00 -0200

    fix bug

--- main.c
+++ main.c
@@ -3,6 +3,14 @@
 #include <time.h>
 #include <windows.h>
 
+typedef short _stdcall (*PtrInp)(short EndPorta);
+typedef void _stdcall (*PtrOut)(short EndPorta, short valor);
+
+HINSTANCE hLib; //Instância para a DLL inpout32.dll.
+PtrInp inportb;     //Instância para a função Imp32().
+PtrOut outportb;  //Instância para a função Out32().
+
+
 void bemVindo(); 	// prototipo da funcao de boas vindas
 void menuPrincipal();
 void recebeVariaveis(); // prototipo da funcao input sequencia
@@ -15,18 +23,22 @@ int main(){
 	if(carregaLib() == -1) return -1;
 	short escolha = 0;
 	while(1){
+		outportb(0x378, 0);
 		system("cls");
 		menuPrincipal();
 		scanf("%d", &escolha);
-		if(escolha == 1) recebeVariaveis();
-		else if(escolha = 2) break;
-		else printf("Escolha invalida");
+		if(escolha == 1) recebeVariaveis(0);
+		else if(escolha == 2) recebeVariaveis(1);
+		else if(escolha == 3) break;
+		else{
+			printf("Escolha invalida");
+			atrasoFuc(50);
+		}
 	}
 	system("exit");
-	
+
 	return 0;
 }
-
 /** Funcao de boas vindas **/
 void bemVindo(){
 	system("title CONTROLE PORTA PARALELA"); // Muda o titulo da janela
@@ -38,12 +50,8 @@ void bemVindo(){
 	}
 	system("cls");
 }
+/** funcao que carrega input32.dll **/
 int carregaLib(){
-	typedef short _stdcall (*PtrInp)(short EndPorta);
-	typedef void _stdcall (*PtrOut)(short EndPorta, short valor);
-	HINSTANCE hLib; //Instância para a DLL inpout32.dll.
-   	PtrInp inportb;     //Instância para a função Imp32(). 
-   	PtrOut outportb;  //Instância para a função Out32().
 
    //Carrega a DLL na memória.
    hLib = LoadLibrary("inpout32.dll");
@@ -79,11 +87,12 @@ int carregaLib(){
 void menuPrincipal(){
 	printf("escolha\t\tfuncao\n\n"); // Cabecalho do menu Inicial
 	/*** Itens do menu ***/
-	printf("1\t\tIniciar sequencia\n");
-	printf("2\t\tSair\n");
+	printf("1\t\tIniciar sequencia em decimal\n");
+	printf("2\t\tIniciar sequencia em binario\n");
+	printf("3\t\tSair\n");
 }
 /** Funcao para definir a sequencia de LEDs acesos **/
-void recebeVariaveis(){
+void recebeVariaveis(int binario){
 	system("cls");
 	int quantPassos = 0; // Variavel que ira receber a quantidade de passos
 	printf("Digite a quantidade de passos: "); // Mensagem com informacao para usuario
@@ -95,10 +104,20 @@ void recebeVariaveis(){
 		passos[i] = 0;
 	}
 	/*** Loop para armazenar cada passo ***/
-	for(i = 0; i < quantPassos; i++){
-			printf("Digite o %dº passo: ", i);
+	if(binario == 0)
+		for(i = 0; i < quantPassos; i++){
+			do{
+			printf("Digite o %d passo (menor  que 255): ", i + 1);
 			scanf("%d", &passos[i]);
+		}while(passos[i] > 255);
 	}
+	else
+		for(i = 0; i < quantPassos; i++){
+			printf("Digite o %d passo (8 bits): ", i);
+			int valorBinario;
+			scanf("%d", &valorBinario);
+			passos[i] = converteParaDecimal(valorBinario);
+		}
 	int atraso = 0; // Variavel para receber o tempo entre cada passo
 	printf("Digite o intervalo entre cada passo (em ms, e apenas inteiros): ");
 	scanf("%d", &atraso);
@@ -107,14 +126,30 @@ void recebeVariaveis(){
 	scanf("%d", &repeticao);
 	sequencia(quantPassos, passos, atraso, repeticao);
 }
+// ** funcao que converte de binario para decimal **/
+int converteParaDecimal(int valorBinario){
+	int valorDecimal = 0;
+	int potencia = 1;
+	while(valorBinario > 0){
+		valorDecimal += valorBinario % 10 * potencia;
+		valorBinario /= 10;
+		potencia *= 2;
+	}
+	return valorDecimal;
+}
 /** funcao que faz sequencia **/
 void sequencia(int quantPassos, int passos[], int atraso, int repeticao){
 	int i, j;
-	for(i = 0; i < repeticao; i++)
+	system("cls");
+	for(i = 0; i < repeticao; i++){
+		printf("Sequencia em progresso");
 		for(j = 0; j < quantPassos; j++){
+			if(j % 3 == 0) printf(".");
 			outportb(0x378, passos[j]);
 			atrasoFuc(atraso);
 		}
+		system("cls");
+	}
 }
 /** funcao de atraso **/
 void atrasoFuc(int atraso){
@@ -123,6 +158,6 @@ void atrasoFuc(int atraso){
 	Ini = clock(); //Inicializacao do relogio
 	 //Executa o while por 10ms antes de retornar o valor
 	while(((clock() - Ini) / (CLOCKS_PER_SEC / 100)) <= atraso){
-			x++;
+		x++;
 	}
 }
